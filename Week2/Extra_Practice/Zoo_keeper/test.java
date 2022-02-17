ea8036d99d5000c342c39012ad73a28b403f8e5a
EthanHaag <92204265+EthanHaag@users.noreply.github.com>
2022-02-17 10:53:07 -0700

    renamed some files

---
+++ Week2/Extra_Practice/Zoo_keeper/test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+        gorilla Gorilla = new gorilla(50);
+        Gorilla.throwSomething();
+        Gorilla.throwSomething();
+        Gorilla.throwSomething();
+        Gorilla.eatBananas();
+        Gorilla.eatBananas();
+        Gorilla.climb();
+    }
+}
